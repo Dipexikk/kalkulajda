26bb57eb14f0e14369f13bd4b78158263ba7bc3c
dipex@seznam.cz <dipex@seznam.cz>
2025-11-21 11:36:08 +0100

    dfgdf

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-let tlacitkoElement = document.getElementById("tlacitko");
-let tlacitko1Element = document.getElementById("tlacitko1");
-let tlacitko2Element = document.getElementById("tlacitko2");
+function to_display(character){
+    console.log('to_display: ');
+    on_display = document.getElementById('id_display').value;
+    on_display = on_display + character.value;
+    document.getElementById('id_display').value = on_display;
+}
+ 
+let expression = '';
+ 
+function operator(character){
+    console.log('operator: ');
+    on_display = document.getElementById('id_display').value;
+    expression = expression + on_display + character.value;
+    document.getElementById('id_display').value = '';
+    console.log(expression);
+}
+
+function result(){
+    console.log('result: ');
+    let on_display = document.getElementById('id_display').value;
+    expression = expression + on_display;
+    console.log(expression);    
+    let to_display = eval(expression);
+    document.getElementById('id_display').value = to_display;
+    expression = '';   
+} 
